5a04cad0a496aa8e83e2dcd5f5dbf09be3d6dadc
Denis <denispolasek@gmail.com>
2023-10-29 15:22:28 +0100

    wip

---
+++ src/components/CustomizableText.vue
@@ -0,0 +1,5 @@
+<template>
+   <!-- PEPA -->
+   <slot></slot>
+</template>
+
--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,13 @@ import {ViteSSGOptions} from "vite-ssg";
 
 const ssgOption:ViteSSGOptions = {
   dirStyle:"nested",
+
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{outDir:'./dist'},
+  build:{outDir:'./dist',},
+
   plugins: [
     vue(),
   ],
